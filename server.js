1e33ec1e37145a321bebb9adbb90a2bfec4ecebe
Kristopher Williams <kristopher.williams2@gmail.com>
2019-10-10 20:29:08 -0500

    finished moviedex

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const MOVIES = require('./movies.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken ){
+        return res.status(401).json({Error: 'Unauthorized Request'})
+    }
+
+    next()
+})
+
+app.get('/movie', function handleGetMovies (req, res)  {
+    let response = MOVIES
+    console.log(req.query.avg_vote)    
+
+    if(req.query.genre){
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+
+    if(req.query.country){
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if(req.query.avg_vote){
+        response = response.filter(movie => 
+            movie.avg_vote >= Number(req.query.avg_vote)
+        ).sort((a, b) => { return a.avg_vote < b.avg_vote ? -1 : a.avg_vote > b.avg_vote ? 1:0
+        })
+    }
+
+res.json(response)
+
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
