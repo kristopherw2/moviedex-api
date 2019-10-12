ffb53c0ab178894551471a2fc4c882700e8fd321
Kristopher Williams <kristopher.williams2@gmail.com>
2019-10-12 10:14:45 -0500

    ready for deployment to heroku

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require('cors')
 const MOVIES = require('./movies.json')
 
 const app = express()
-
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+ app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -24,7 +24,6 @@ app.use(function validateBearerToken(req, res, next){
 
 app.get('/movie', function handleGetMovies (req, res)  {
     let response = MOVIES
-    console.log(req.query.avg_vote)    
 
     if(req.query.genre){
         response = response.filter(movie =>
@@ -49,8 +48,18 @@ res.json(response)
 
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+
 })
